48fce59f813150887081184f6a14168efaa8bced
Alvin Abhinav <alvin.abhinav@ithands.net>
2019-11-14 12:12:14 +0530

    first commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -49,10 +49,19 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 GRAPHENE ={
-    'SCHEMA':'app.schema.schema'
+    'SCHEMA':'app.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 ROOT_URLCONF = 'app.urls'
 
--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -22,9 +22,27 @@ class CreateTrack(graphene.Mutation):
         url =graphene.String()
         
     def mutate(self,info,title,description,url):
-        track =Track(title=title,description=description,url=url)
+        user=info.context.user
+        if user.is_anonymous:
+            raise Exception('Log in to add a trak ')
+        track =Track(title=title,description=description,url=url,posted_by=user)
         track.save()
         return CreateTrack(track=track)    
     
+class DeleteTrack(graphene.Mutation):
+    track_id = graphene.Field(TrackType)
+    class Arguments:
+        track_id=graphene.Int(required=True)
+        
+    def mutate(self,info,track_id):
+        # user= info.context.user
+        track =Track.objects.get(id=track_id)
+        track.delete()
+        return DeleteTrack(track_id=track_id)
+
+
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    # update_track = UpdateTrack.Field()
+    delete_track = DeleteTrack.Field()
